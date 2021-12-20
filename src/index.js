7348738813411154dcef9ec048bc91f065aef69e
paimdev <bathkelucas@gmail.com>
2021-12-20 12:50:22 -0300

    Create tab's logic and simple test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import home from "./home-component";
 
 const contentContainer = document.getElementById("content");
+const tabContainer = document.createElement("div");
+const tabList = document.createElement("ul");
+const tabContent = document.createElement("div");
 
-contentContainer.appendChild(home());
+const homeTab = document.createElement("li");
+homeTab.innerHTML = "Home";
+const menuTab = document.createElement("li");
+menuTab.innerHTML = "Menu";
+const contactTab = document.createElement("li");
+contactTab.innerHTML = "Contact";
+
+homeTab.addEventListener("click", () => {
+  tabContent.innerHTML = "";
+  tabContent.appendChild(home())
+});
+
+tabList.append(homeTab, menuTab, contactTab);
+
+contentContainer.appendChild(tabList);
+contentContainer.appendChild(tabContent);
